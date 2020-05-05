5e57042c4aad008e0426cb7b217853e06f644cfa
RyotArch <r.yamamoto.system@gmail.com>
2020-05-05 22:27:42 +0900

    Implement Insert

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Tasks } from '../api/tasks.js';
+import ReactDOM from 'react-dom';
 
 const App = (props) => {
   console.log(props)
+
+  const [value, setValue] = useState();
+  const handleSubmit = () => {
+    Tasks.insert({
+      text: value,
+      createdAt: new Date(), // current time
+    });
+    setValue('');
+    event.preventDefault();
+  }
   return (
   <div className="container">
     <header>
       <h1>Todo List</h1>
     </header>
+    <form className="new-task">
+      <input
+        type="text"
+        placeholder="Type to add new tasks"
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button onClick={()=> handleSubmit()}>Send</button>
+    </form>
     <ul>
       {props.tasks.map((task) => (
         <li key={task._id}>{task.text}</li>
@@ -18,7 +37,8 @@ const App = (props) => {
   );
 }
 export default withTracker(() => {
+  // ソート処理を追加
   return {
-    tasks: Tasks.find({}).fetch(),
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
   };
 })(App);
