2ade36f91fd826a76e524f0eefa1997eaeffc665
RyotArch <r.yamamoto.system@gmail.com>
2020-05-05 21:31:35 +0900

    Implement base UI

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
 
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
+const getTasks = () => {
+  return [
+    { _id: 1, text: 'This is task 1' },
+    { _id: 2, text: 'This is task 2' },
+    { _id: 3, text: 'This is task 3' },
+  ];
+}
+
+const Tasks = () => {
+  return (
+    <ul>
+      {getTasks().map((task) => (
+        <li key={task._id}>{task.text}</li>
+      ))}
+    </ul>
+  )
+}
+
+export const App = () => {
+  return (
+  <div className="container">
+    <header>
+      <h1>Todo List</h1>
+    </header>
+    <Tasks />
   </div>
-);
+  );
+}
